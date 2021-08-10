ca0546521936d709e9ba96662e50dd01b611999b
Charlie Weber <charlestweber@gmail.com>
2021-08-10 16:22:44 -0700

    WIP: trying to remove the pig after we insert the pig img

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,15 @@ $("document").ready(function() {
   $("button#dog").click(function(){
     $("ul#doggo").prepend("<li><strong>BARK!</strong></li>");
     $("ul#catto").prepend("<li>*ANGRY* MEOW</li>");
-  });
+    });
   $("button#cat").click(function(){
     $("ul#doggo").prepend("<li><em>sad howl</em></li>");
     $("ul#catto").prepend("<li><strong> HISSSSSSS</strong></li>");
-  });
+    });
   $("ul").click(function(){
-    $(this).after("<li><img src='img/babe.jpg'></li>");
-  });
+    $(this).after("<div id='pig'><img src='img/babe.jpg'></div>");
+    });
+  $("div#pig").children("img").first().click(function() {
+    $(this).remove();
+    });
 });

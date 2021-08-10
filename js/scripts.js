7145af386dbb34151da39bcab7156d023bdc8702
Charlie Weber <charlestweber@gmail.com>
2021-08-10 15:25:20 -0700

    Adds pig to UL once clicked

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,11 @@ $("document").ready(function() {
     $("ul#doggo").prepend("<li><strong>BARK!</strong></li>");
     $("ul#catto").prepend("<li>*ANGRY* MEOW</li>");
   });
+  $("button#cat").click(function(){
+    $("ul#doggo").prepend("<li><em>sad howl</em></li>");
+    $("ul#catto").prepend("<li><strong> HISSSSSSS</strong></li>");
+  });
+  $("ul").click(function(){
+    $(this).after("<li><img src='img/babe.jpg'></li>");
+  });
 });
